294d3bbea4b549ef50a1a83521737225b67de921
naveensands2 <30368815+naveensands2@users.noreply.github.com>
2017-08-29 15:59:04 +0530

    Rename positive.c to positive and negative.c

---
+++ positive and negative.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <conio.h>
+main()
+{
+    int i;
+
+    printf("Enter a number");
+    scanf("%d", &i);
+    if (i<= 0.0)
+    {
+        if (number == 0.0)
+            printf("it is neither positive nor negative");
+        else
+            printf("it is negative");
+    }
+    else
+        printf("it is positive");
+    return ;
+}
